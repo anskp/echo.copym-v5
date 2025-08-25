5828ecc9c2c1e1b3a9ddb7f90ce345046f045af9
Your New Username <your-new-email@example.com>
2025-08-25 19:42:37 +0530

    Own. Earn. Explore. Trade, and added Track your Crypto. Anywhere.

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,19 @@
-import MarketplacePreview from "./sections/MarketplacePreview";
 import Hero from "./sections/Hero";
-import HowItWorks from "./sections/HowItWorks";
+import CurvedSectionDivider from "./sections/CurvedSectionDivider";
+import TrackYourCrypto from "./sections/TrackYourCrypto";
 import IssuerSection from "./sections/IssuerSection";
 import WhyCopym from "./sections/WhyCopym";
-import InvestmentSection from "./sections/InvestmentSection";
 import Testimonials from "./sections/Testimonials";
 import SupportedAssets from "./sections/SupportedAssets";
 import FAQ from "./sections/FAQ";
+
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-blue-100">
       <Hero />
-      <HowItWorks />
-      <InvestmentSection />
+      <CurvedSectionDivider />
+      <TrackYourCrypto />
       {/* <SupportedAssets /> */}
-      <MarketplacePreview />
       <IssuerSection />
       <WhyCopym />
       <Testimonials />
