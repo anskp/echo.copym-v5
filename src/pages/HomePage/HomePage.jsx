4150647f25f8d840da22266e7e71eef64fecb43b
Your New Username <your-new-email@example.com>
2025-08-26 03:37:58 +0530

    added FAQs

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import CurvedSectionDivider from "./sections/CurvedSectionDivider";
 import TrackYourCrypto from "./sections/TrackYourCrypto";
 import AIInvestingSection from "./sections/AIInvestingSection";
 import AuditSection from "./sections/AuditSection";
+import FAQSection from "./sections/FAQSection";
 import NewsletterSection from "./sections/NewsletterSection";
 import Testimonials from "./sections/Testimonials";
 import SupportedAssets from "./sections/SupportedAssets";
@@ -15,6 +16,7 @@ export default function HomePage() {
       <TrackYourCrypto />
       <AIInvestingSection />
       <AuditSection />
+      <FAQSection />
       <NewsletterSection />
       {/* <SupportedAssets /> */}
       <Testimonials />
