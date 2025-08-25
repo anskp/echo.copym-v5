6f98bd82a5885cacad7be179280e237b3fe56fb1
nadabd1234 <4pa21cs089@pace.edu.in>
2025-08-26 00:16:49 +0530

    contact us page removed standard form

--- src/pages/CopymAI/components/CompanyLogos.jsx
+++ src/pages/CopymAI/components/CompanyLogos.jsx
@@ -14,10 +14,10 @@ const CompanyLogos = ({ className }) => {
           {companyLogos.map((logo, i) => (
             <div
               key={`first-${i}`}
-              className="flex-shrink-0 mx-2 md:mx-4 animate-rotate-coin"
+              className="flex-shrink-0 mx-3 md:mx-6 animate-rotate-coin"
               style={{ animationDelay: `${i * 0.3}s` }}
             >
-              <div className="relative w-12 h-12 md:w-16 md:h-16">
+              <div className="relative w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24">
                 {/* Outer ring - thin line layer */}
                 <div className="absolute inset-0 rounded-full border-2 border-gray-300 bg-white shadow-lg"></div>
                 
@@ -25,10 +25,10 @@ const CompanyLogos = ({ className }) => {
                 <div className="absolute inset-1 rounded-full bg-gradient-to-br from-blue-50 to-gray-50 flex items-center justify-center overflow-hidden">
                   <img 
                     src={logo} 
-                    width={32} 
-                    height={32} 
+                    width={48} 
+                    height={48} 
                     alt={`Crypto-${i}`}
-                    className="w-6 h-6 md:w-8 md:h-8 object-contain"
+                    className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 object-contain"
                   />
                 </div>
                 
@@ -42,10 +42,10 @@ const CompanyLogos = ({ className }) => {
           {companyLogos.map((logo, i) => (
             <div
               key={`second-${i}`}
-              className="flex-shrink-0 mx-2 md:mx-4 animate-rotate-coin"
+              className="flex-shrink-0 mx-3 md:mx-6 animate-rotate-coin"
               style={{ animationDelay: `${i * 0.3}s` }}
             >
-              <div className="relative w-12 h-12 md:w-16 md:h-16">
+              <div className="relative w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24">
                 {/* Outer ring - thin line layer */}
                 <div className="absolute inset-0 rounded-full border-2 border-gray-300 bg-white shadow-lg"></div>
                 
@@ -53,10 +53,10 @@ const CompanyLogos = ({ className }) => {
                 <div className="absolute inset-1 rounded-full bg-gradient-to-br from-blue-50 to-gray-50 flex items-center justify-center overflow-hidden">
                   <img 
                     src={logo} 
-                    width={32} 
-                    height={32} 
+                    width={48} 
+                    height={48} 
                     alt={`Crypto-${i}`}
-                    className="w-6 h-6 md:w-8 md:h-8 object-contain"
+                    className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 object-contain"
                   />
                 </div>
                 
