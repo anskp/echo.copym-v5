92a1e40c4ed39a42186d1f042e28b8e81c2b7175
nadabd1234 <4pa21cs089@pace.edu.in>
2025-10-10 20:12:11 +0530

    added 4 layouts

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,4 @@
 import Hero from "./sections/Hero";
-import CurvedSectionDivider from "./sections/CurvedSectionDivider";
 import TrackYourCrypto from "./sections/TrackYourCrypto";
 import AIInvestingSection from "./sections/AIInvestingSection";
 import RWAAssetsSection from "./sections/RWAAssetsSection";
@@ -8,12 +7,24 @@ import FAQSection from "./sections/FAQSection";
 import NewsletterSection from "./sections/NewsletterSection";
 import Testimonials from "./sections/Testimonials";
 import SupportedAssets from "./sections/SupportedAssets";
+import FourLayouts from "../../components/SVG/4layouts.svg";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-blue-100 p-0 m-0">
       <Hero />
-      <CurvedSectionDivider />
+      {/* Four Layouts SVG Section */}
+      <section className="py-4 sm:py-6 lg:py-8">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-center items-center">
+            <img 
+              src={FourLayouts} 
+              alt="Four Layout Features" 
+              className="w-full max-w-6xl h-auto"
+            />
+          </div>
+        </div>
+      </section>
       <TrackYourCrypto />
       <AIInvestingSection />
       <RWAAssetsSection />
