3e1da405104bb3e69218d19cfd6d6bc844694daf
nadabd1234 <4pa21cs089@pace.edu.in>
2025-10-16 08:28:41 +0530

    animated the homecycle

---
+++ src/components/ui/Geometric.jsx
@@ -0,0 +1,191 @@
+import React from 'react';
+
+export default function GeometricDesign() {
+  return (
+    <div className="relative w-full h-screen bg-gray-50 overflow-hidden" style={{ minHeight: '1000px', minWidth: '2200px' }}>
+      {/* Polygon 5 */}
+      <div 
+        className="absolute"
+        style={{
+          width: '314.42px',
+          height: '312.63px',
+          left: '581px',
+          top: '298px',
+          background: 'radial-gradient(63.92% 64.28% at 50% 50%, rgba(189, 229, 214, 0.7) 0%, rgba(255, 255, 255, 0.7) 100%)',
+          transform: 'rotate(-30deg)'
+        }}
+      />
+
+      {/* Polygon 9 */}
+      <div 
+        className="absolute"
+        style={{
+          width: '314.42px',
+          height: '312.63px',
+          left: '1515px',
+          top: '305px',
+          background: 'radial-gradient(63.92% 64.28% at 50% 50%, rgba(189, 229, 214, 0.7) 0%, rgba(255, 255, 255, 0.7) 100%)',
+          transform: 'rotate(-30deg)'
+        }}
+      />
+
+      {/* Ellipse 666 */}
+      <div 
+        className="absolute"
+        style={{
+          width: '707px',
+          height: '707px',
+          left: '1373px',
+          top: '148px',
+          boxSizing: 'border-box'
+        }}
+      />
+
+      {/* Ellipse 667 */}
+      <div 
+        className="absolute"
+        style={{
+          width: '508px',
+          height: '508px',
+          left: '1392.45px',
+          top: '167.45px',
+          transform: 'rotate(156.35deg)',
+          boxSizing: 'border-box'
+        }}
+      />
+
+      {/* Lines */}
+      <div className="absolute" style={{ width: '377.6px', height: '0px', left: '1773px', top: '129px', border: '1px dashed #15A36E', transform: 'rotate(-52.16deg)' }} />
+      <div className="absolute" style={{ width: '367.9px', height: '0px', left: '1467px', top: '139px', border: '1px solid #255F99', transform: 'rotate(-126.03deg)' }} />
+      <div className="absolute" style={{ width: '674px', height: '0px', left: '1553px', top: '509px', border: '4px solid #4C83B9', transform: 'matrix(1, 0, 0, -1, 0, 0)' }} />
+      <div className="absolute" style={{ width: '363.08px', height: '0px', left: '1524px', top: '577px', border: '1px dashed #255F99', transform: 'rotate(116.12deg)' }} />
+      <div className="absolute" style={{ width: '377.6px', height: '0px', left: '1773px', top: '577px', border: '1px solid #15A36E', transform: 'matrix(0.55, 0.84, 0.84, -0.55, 0, 0)' }} />
+
+      {/* Small Ellipses - Right Side */}
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '2062px', top: '543px', background: 'radial-gradient(50% 50% at 50% 50%, #15A36E 0%, #2983DE 100%)' }} />
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '1773px', top: '837px', background: '#000000' }} />
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '1558px', top: '680px', background: 'radial-gradient(50% 50% at 50% 50%, #15A36E 0%, #2983DE 100%)' }} />
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '1727px', top: '242px', background: '#000000' }} />
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '1905px', top: '656px', background: '#000000' }} />
+
+      {/* Polygon 4 */}
+      <div 
+        className="absolute"
+        style={{
+          width: '175px',
+          height: '174px',
+          left: '676px',
+          top: '387px',
+          background: '#2983DE',
+          transform: 'rotate(-30deg)'
+        }}
+      />
+
+      {/* Ellipse 674 */}
+      <div className="absolute" style={{ width: '707px', height: '707px', left: '441px', top: '152px', boxSizing: 'border-box' }} />
+
+      {/* Ellipse 689 */}
+      <div 
+        className="absolute rounded-full"
+        style={{
+          width: '707px',
+          height: '707px',
+          left: '323.49px',
+          top: '71.49px',
+          background: '#15A36E',
+          transform: 'rotate(20.16deg)'
+        }}
+      />
+
+      {/* Ellipse 675 */}
+      <div className="absolute" style={{ width: '508px', height: '508px', left: '460.45px', top: '171.45px', transform: 'rotate(156.35deg)', boxSizing: 'border-box' }} />
+
+      {/* More Lines - Left Side */}
+      <div className="absolute" style={{ width: '370.64px', height: '0px', left: '841px', top: '139px', border: '1px dashed #15A36E', transform: 'rotate(-52.04deg)' }} />
+      <div className="absolute" style={{ width: '674px', height: '0px', left: '879px', top: '513px', border: '4px solid #4C83B9' }} />
+      <div className="absolute" style={{ width: '360.66px', height: '0px', left: '536px', top: '142px', border: '1px solid #255F99', transform: 'matrix(-0.6, -0.8, -0.8, 0.6, 0, 0)' }} />
+      <div className="absolute" style={{ width: '377.6px', height: '0px', left: '329px', top: '506px', border: '1px solid #15A36E', transform: 'matrix(1, 0, 0, -1, 0, 0)' }} />
+      <div className="absolute" style={{ width: '377.6px', height: '0px', left: '587px', top: '581px', border: '1px dashed #255F99', transform: 'matrix(-0.44, 0.9, 0.9, 0.44, 0, 0)' }} />
+      <div className="absolute" style={{ width: '382.26px', height: '0px', left: '841px', top: '581px', border: '1px solid #15A36E', transform: 'rotate(57.39deg)' }} />
+
+      {/* Small Ellipses - Left Side */}
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '568px', top: '625px', background: '#000000' }} />
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '943px', top: '184px', background: '#000000' }} />
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '795px', top: '246px', background: 'radial-gradient(50% 50% at 50% 50%, #15A36E 0%, #2983DE 100%)' }} />
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '972px', top: '656px', background: 'radial-gradient(50% 50% at 50% 50%, #15A36E 0%, #2983DE 100%)' }} />
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '1319px', top: '246px', background: 'radial-gradient(50% 50% at 50% 50%, #15A36E 0%, #2983DE 100%)' }} />
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '493px', top: '295px', background: 'radial-gradient(50% 50% at 50% 50%, #15A36E 0%, #2983DE 100%)' }} />
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '1368px', top: '543px', background: '#000000' }} />
+
+      {/* Horizontal Lines */}
+      <div className="absolute" style={{ width: '477px', height: '0px', left: '1047px', top: '904px', border: '1px solid #15A36E' }} />
+      <div className="absolute" style={{ width: '394px', height: '0px', left: '1073px', top: '139px', border: '1px solid #15A36E' }} />
+
+      {/* Vector Groups - Bottom */}
+      <div className="absolute" style={{ width: '470px', height: '470px', left: '345px', top: '500px', border: '5px dashed #15A36E', borderRadius: '5px', transform: 'rotate(180deg)' }} />
+      <div className="absolute" style={{ width: '470px', height: '470px', left: '787.93px', top: '500px', border: '5px dashed #15A36E', borderRadius: '5px', transform: 'matrix(1, 0, 0, -1, 0, 0)' }} />
+
+      {/* Vector Groups - Top */}
+      <div className="absolute" style={{ width: '470px', height: '465px', left: '1701.93px', top: '48px', border: '5px dashed #15A36E', borderRadius: '5px' }} />
+      <div className="absolute" style={{ width: '470px', height: '465px', left: '1259px', top: '48px', border: '5px dashed #15A36E', borderRadius: '5px', transform: 'matrix(-1, 0, 0, 1, 0, 0)' }} />
+
+      {/* Polygon 3 */}
+      <div className="absolute" style={{ width: '175px', height: '174px', left: '1608px', top: '393px', background: '#2983DE', transform: 'rotate(-30deg)' }} />
+
+      {/* Vector Arrows */}
+      <div className="absolute" style={{ width: '38.26px', height: '23.31px', left: '1494px', top: '758px', border: '3px solid #000000', borderRadius: '5px', transform: 'rotate(129.66deg)' }} />
+      <div className="absolute" style={{ width: '38.26px', height: '23.31px', left: '1796.04px', top: '711.65px', border: '3px solid #000000', borderRadius: '5px', transform: 'rotate(55.65deg)' }} />
+      <div className="absolute" style={{ width: '38.26px', height: '23.31px', left: '1368px', top: '394px', border: '3px solid #000000', borderRadius: '5px', transform: 'rotate(-163.18deg)' }} />
+      <div className="absolute" style={{ width: '38.26px', height: '23.31px', left: '1025.65px', top: '473.64px', border: '3px solid #000000', borderRadius: '5px', transform: 'rotate(176.7deg)' }} />
+
+      {/* Ellipse 690 */}
+      <div className="absolute rounded-full" style={{ width: '707px', height: '707px', left: '1255px', top: '0px', background: '#15A36E', transform: 'rotate(148.35deg)' }} />
+
+      {/* More Vector Arrows */}
+      <div className="absolute" style={{ width: '38.26px', height: '23.31px', left: '817px', top: '735px', border: '3px solid #000000', borderRadius: '5px', transform: 'rotate(-104.95deg)' }} />
+      <div className="absolute" style={{ width: '38.26px', height: '23.31px', left: '568px', top: '338px', border: '3px solid #000000', borderRadius: '5px', transform: 'rotate(43.86deg)' }} />
+      <div className="absolute" style={{ width: '38.26px', height: '23.31px', left: '1913.52px', top: '346.1px', border: '3px solid #000000', borderRadius: '5px', transform: 'rotate(-19.1deg)' }} />
+
+      {/* Additional Small Ellipses */}
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '1123px', top: '822px', background: '#000000' }} />
+      <div className="absolute rounded-full" style={{ width: '24px', height: '24px', left: '1129px', top: '553px', background: '#000000' }} />
+
+      {/* Investment & Innovation Hexagons */}
+      <div className="absolute" style={{ width: '707px', height: '707px', left: '1240.94px', top: '40.94px', background: '#FFFFFF', transform: 'rotate(30.62deg)' }} />
+      <div className="absolute" style={{ width: '707px', height: '707px', left: '368.1px', top: '51.1px', background: '#FFFFFF', transform: 'rotate(-108.05deg)' }} />
+
+      {/* Large Gradient Circles */}
+      <div className="absolute rounded-full" style={{ width: '96px', height: '96px', left: '550px', top: '174.5px', background: 'radial-gradient(38.16% 38.16% at 50% 50%, #7DD1B2 0%, #FFFFFF 100%)', boxSizing: 'border-box' }} />
+      <div className="absolute rounded-full" style={{ width: '96px', height: '96px', left: '969px', top: '357px', background: 'radial-gradient(38.16% 38.16% at 50% 50%, #7DD1B2 0%, #FFFFFF 100%)', boxSizing: 'border-box' }} />
+      <div className="absolute rounded-full" style={{ width: '96px', height: '96px', left: '669px', top: '793px', background: 'radial-gradient(38.16% 38.16% at 50% 50%, #7DD1B2 0%, #FFFFFF 100%)', boxSizing: 'border-box' }} />
+      <div className="absolute rounded-full" style={{ width: '96px', height: '96px', left: '1476px', top: '174px', background: 'radial-gradient(38.16% 38.16% at 50% 50%, #7DD1B2 0%, #FFFFFF 100%)', boxSizing: 'border-box' }} />
+      <div className="absolute rounded-full" style={{ width: '96px', height: '96px', left: '1914px', top: '471px', background: 'radial-gradient(38.16% 38.16% at 50% 50%, #7DD1B2 0%, #FFFFFF 100%)', boxSizing: 'border-box' }} />
+      <div className="absolute rounded-full" style={{ width: '96px', height: '96px', left: '1392px', top: '655px', background: 'radial-gradient(38.16% 38.16% at 50% 50%, #7DD1B2 0%, #FFFFFF 100%)', boxSizing: 'border-box' }} />
+
+      {/* Icon Placeholders - Replace with actual icons/images */}
+      <div className="absolute flex items-center justify-center rounded-full bg-white" style={{ width: '47.92px', height: '47.92px', left: '991.79px', top: '381.19px' }}>
+        <span className="text-xs">🎯</span>
+      </div>
+
+      <div className="absolute flex items-center justify-center rounded-full bg-white" style={{ width: '47px', height: '47px', left: '1501px', top: '199px' }}>
+        <span className="text-xs">💡</span>
+      </div>
+
+      <div className="absolute flex items-center justify-center rounded-full bg-white" style={{ width: '91px', height: '91px', left: '1919px', top: '473px' }}>
+        <span className="text-2xl">📊</span>
+      </div>
+
+      <div className="absolute flex items-center justify-center rounded-full bg-white" style={{ width: '56px', height: '56px', left: '1412px', top: '675px' }}>
+        <span className="text-sm">💼</span>
+      </div>
+
+      <div className="absolute flex items-center justify-center rounded-full bg-white" style={{ width: '47.23px', height: '47.23px', left: '574.31px', top: '197.56px' }}>
+        <span className="text-xs">🚀</span>
+      </div>
+
+      <div className="absolute flex items-center justify-center rounded-full bg-black" style={{ width: '50px', height: '50px', left: '692px', top: '816px' }}>
+        <span className="text-white text-xs">📈</span>
+      </div>
+    </div>
+  );
+}
--- src/pages/HomePage/sections/Hero.jsx
+++ src/pages/HomePage/sections/Hero.jsx
@@ -1,7 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GiPlayButton, GiCrossedSwords } from 'react-icons/gi';
 import { Link } from 'react-router-dom';
 import HomeCycle from '../../../components/SVG/HomeCycle.svg';
+import HomeCycle1 from '../../../components/SVG/HomeCycle1.svg';
+import HomeCycle2 from '../../../components/SVG/HomeCycle2.svg';
+import HomeCycle3 from '../../../components/SVG/HomeCycle3.svg';
+import HomeCycle4 from '../../../components/SVG/HomeCycle4.svg';
+import HomeCycle5 from '../../../components/SVG/HomeCycle5.svg';
+import HomeCycle6 from '../../../components/SVG/HomeCycle6.svg';
 
 // Realistic iPhone Component for Hero - First Phone (Marketplace)
 const HeroPhone3D = () => {
@@ -117,6 +123,37 @@ const HeroPhone3DWallet = () => {
 
 export default function Hero() {
   const [isVideoOpen, setVideoOpen] = useState(false);
+  const [currentSVGIndex, setCurrentSVGIndex] = useState(0);
+  
+  // Array of all HomeCycle SVG files (excluding original HomeCycle.svg)
+  const homeCycleSVGs = [
+    HomeCycle1,
+    HomeCycle2,
+    HomeCycle3,
+    HomeCycle4,
+    HomeCycle5,
+    HomeCycle6
+  ];
+
+  // Preload all images for smoother transitions
+  useEffect(() => {
+    homeCycleSVGs.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+    });
+  }, []);
+
+  // Auto-rotate through SVG files matching Figma prototype settings
+  // Delay: 1ms, Duration: 300ms, Total: 301ms per frame
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSVGIndex((prevIndex) => 
+        (prevIndex + 1) % homeCycleSVGs.length
+      );
+    }, 301); // 1ms delay + 300ms duration = 301ms per frame
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="relative overflow-visible">
@@ -164,14 +201,24 @@ export default function Hero() {
           </div>
         </div>
 
-        {/* HomeCycle SVG Section */}
+        {/* HomeCycle SVG Section with Rotating Animation */}
         <div className="mt-20 mb-12">
-          <div className="flex justify-center items-center">
-            <img 
-              src={HomeCycle} 
-              alt="Innovation and Investment Cycle" 
-              className="w-full max-w-4xl h-auto"
-            />
+          <div className="flex justify-center items-center w-full">
+            <div className="relative w-full max-w-4xl h-auto flex justify-center items-center">
+              {/* Single image that changes src rapidly */}
+              <img 
+                src={homeCycleSVGs[currentSVGIndex]} 
+                alt="Innovation and Investment Cycle" 
+                className="w-full h-auto mx-auto"
+                style={{
+                  maxWidth: '100%',
+                  width: '100%',
+                  height: 'auto',
+                  display: 'block',
+                  margin: '0 auto'
+                }}
+              />
+            </div>
           </div>
         </div>
 
