ba11f162a15e348af08f0ac50465340029d2834e
Fathimaath <nuhafathima761@gmail.com>
2026-04-20 16:19:33 +0530

    Added SSG pre-rendering for SEO

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,32 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import fs from 'fs';
+import SSG from 'vite-plugin-ssg';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    SSG({
+      routes: [
+        '/',
+        '/about',
+        '/marketplace',
+        '/tokenization',
+        '/blog',
+        '/glossary',
+        // Blog posts
+        '/blog/education/understanding-rwa-tokenization',
+        '/blog/education/blockchain-fundamentals',
+        '/blog/insights/tokenization-trends-2024',
+        // Glossary terms
+        '/glossary/blockchain',
+        '/glossary/tokenization',
+        '/glossary/defi',
+      ],
+      format: 'json',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
