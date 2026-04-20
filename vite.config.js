16da7ea61d46c354fd536caab4c9f6db557bcf29
Fathimaath <nuhafathima761@gmail.com>
2026-04-20 16:34:09 +0530

    added ssg pre rendering

--- vite.config.js
+++ vite.config.js
@@ -17,10 +17,6 @@ export default defineConfig({
         '/blog',
         '/glossary',
       ],
-      crawlerLinks: [
-        '/blog/**',
-        '/glossary/**',
-      ],
     }),
   ],
   resolve: {
