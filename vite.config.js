70f08b2f87c90c7746e0d2dbaf88bf6f49933c48
Fathimaath <nuhafathima761@gmail.com>
2026-04-20 16:39:33 +0530

    Revert SSG config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import fs from 'fs';
-import SSG from 'vite-plugin-ssg';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    SSG({
-      routes: [
-        '/',
-        '/about',
-        '/marketplace',
-        '/tokenization',
-        '/blog',
-        '/glossary',
-      ],
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
